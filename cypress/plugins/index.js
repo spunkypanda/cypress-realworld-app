6aaa1962bdee8c2a22674fc3d0e39c9f47c77de1
Kevin Old <kevin@kevinold.com>
2020-05-05 11:44:02 -0500

    overload "require" as esm require for plugins to allow import of ES6 module in plugins

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,12 @@
 // const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
+// eslint-disable-next-line no-native-reassign
+require = require("esm")(module); // eslint-disable-line no-global-assign
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const { find, filter } = require("lodash/fp");
 const axios = require("axios").default;
+const awsConfig = require("../../src/aws-exports").default;
 
 require("dotenv").config();
 
@@ -11,6 +14,7 @@ module.exports = (on, config) => {
   config.env.defaultPassword = process.env.SEED_DEFAULT_USER_PASSWORD;
   config.env.paginationPageSize = process.env.PAGINATION_PAGE_SIZE;
   config.env.mobileViewportWidth = process.env.MOBILE_VIEWPORT_WIDTH;
+  config.env.awsUserPoolsId = awsConfig.aws_user_pools_id;
 
   // on("file:preprocessor", cypressTypeScriptPreprocessor);
   // `on` is used to hook into various events Cypress emits
